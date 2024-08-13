08d8a47d0c531dc36b25567a6c3ed6ae527f525a
Athul-Sagar <athulsagar@gmail.com>
2024-08-14 02:12:17 +0530

    Initial commit for furniture website

---
+++ script.js
@@ -0,0 +1,9 @@
+let menu=document.querySelector('.menu_icon');
+let navbar=document.querySelector('.navbar');
+
+
+menu.onclick= ()=> {
+    navbar.classList.toggle('active');
+}
+
+console.log("Hello World");
